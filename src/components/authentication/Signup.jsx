b99e26c851f14f89286706f76fa1d77b7d53de5a
gom3a <mohamed.mido85@azhar.edu.eg>
2021-01-07 17:25:59 +0200

    committing

--- src/components/authentication/Signup.jsx
+++ src/components/authentication/Signup.jsx
@@ -2,25 +2,88 @@ import React, { Component } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { Container, Col, Row } from "react-bootstrap";
+import SignBtn from "../btns/SignBtn";
+import axios from "axios";
+
 class Signup extends Component {
- state = {};
+ state = {
+  fname: "",
+  password: "",
+  lname: "",
+  email: "",
+  password_confirmation: "Muhamed10",
+ };
+
+ handleSubmit = (e) => {
+  e.preventDefault();
+  console.log("clicked!");
+  console.log(this.state);
+
+  axios
+   .post(
+    "https://arch17.herokuapp.com/api/user/registration/signup",
+    this.state
+   )
+   .then((res) => {
+    console.log(res.data.Bearer_token);
+    localStorage.setItem("token", res.data.Bearer_token);
+   });
+ };
  render() {
   return (
-   <div id="wrapper" className="">
-    <Container>
+   <div id="wrapper">
+    <Container fluid>
      <Row className="justify-content-md-center">
-      <Col md={{ span: 6 }}>
-       <Form>
+      <Col md={{ span: 7 }} sm={{ span: 8 }} className="m-auto">
+       <div className="heading">
+        <h2>Welcome to Arch17</h2>
+        <h6>The world platform for architecture & design</h6>
+       </div>
+       <Form onSubmit={this.handleSubmit}>
+        <Form.Group>
+         <Form.Row>
+          <Col>
+           <Form.Control
+            placeholder="First name"
+            onChange={(e) => {
+             this.state.fname = e.target.value;
+            }}
+           />
+          </Col>
+          <Col>
+           <Form.Control
+            placeholder="Last name"
+            onChange={(e) => {
+             this.state.lname = e.target.value;
+            }}
+           />
+          </Col>
+         </Form.Row>
+        </Form.Group>
         <Form.Group controlId="formBasicEmail">
-         <Form.Control type="email" placeholder="Enter email" />
+         <Form.Control
+          type="email"
+          placeholder="Enter email"
+          onChange={(e) => {
+           this.state.email = e.target.value;
+          }}
+         />
         </Form.Group>
         <Form.Group controlId="formBasicPassword">
-         <Form.Control type="password" placeholder="Password" />
+         <Form.Control
+          type="password"
+          placeholder="Password"
+          onChange={(e) => {
+           this.state.password = e.target.value;
+          }}
+         />
         </Form.Group>
 
-        <Button variant="primary" type="submit">
+        {/* <Button variant="primary" type="submit">
          Submit
-        </Button>
+        </Button> */}
+
+        <SignBtn />
        </Form>
       </Col>
      </Row>
---
+++ src/components/btns/SignBtn.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class SignBtn extends Component {
+ state = {};
+ render() {
+  return (
+   <button className="coninue-btn" type="submit">
+    Continue
+   </button>
+  );
+ }
+}
+
+export default SignBtn;
