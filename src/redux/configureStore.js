9411745fe49a89ecb3d1fb8f526c492ea9fb3bba
gom3a <mohamed.mido85@azhar.edu.eg>
2021-04-07 02:08:51 +0200

    committing

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -4,8 +4,9 @@ import rootReducer from './reducers/index';
 
 import { apiMiddlware } from './middleware';
 
+import thunk from 'redux-thunk'
 
 export default function configureStore(initialState) {
     const composerEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-    return createStore(rootReducer, initialState, composerEnhancers(applyMiddleware(apiMiddlware)))
+    return createStore(rootReducer, initialState, composerEnhancers(applyMiddleware(thunk, apiMiddlware)))
 }
--- src/redux/middleware.js
+++ src/redux/middleware.js
@@ -1,13 +1,11 @@
 import axios from 'axios'
 import * as constants from '../static/constant'
 
+const BASE_URL = 'https://arch17.herokuapp.com/api/';
 
-export const apiMiddlware = ({ dispatch, getState }) => next => action => {
-
-    if (action.type !== constants.API) return next(action);
-
-    const BASE_URL = 'https://arch17.herokuapp.com/api/';
-
+export const apiMiddlware = ({ dispatch }) => next =>  action => {
+    // if (action.type !== constants.API) return next(action);
+if (action.type===constants.API){
     const { url, method, success, data, postProcesSuccess, postProcesError } = action.payload;
     axios({
         method,
@@ -15,6 +13,7 @@ export const apiMiddlware = ({ dispatch, getState }) => next => action => {
         data: data ? data : null
     }).then((response) => {
         // console.log(response)
+
         window.location.reload('/')
         if (success) dispatch(success(response.data));
         if (success) postProcesSuccess(response.data);
@@ -26,5 +25,31 @@ export const apiMiddlware = ({ dispatch, getState }) => next => action => {
             }
         }
     })
-
+}else{
+    return next(action);
 }
+}
+
+// export const AuthRequest = ()=>{
+//     return dispatch => action => {
+//         const { url, method, success, data, postProcesSuccess, postProcesError } = action.payload;
+      
+//         dispatch(registerUser())
+//         axios({
+//             method,
+//             url: BASE_URL + url,
+//             data: data ? data : null
+//         }).then((response) => {
+//             window.location.reload('/')
+//             if (success) dispatch(success(response.data));
+//             if (success) postProcesSuccess(response.data);
+//         }).catch(err => {
+//             if (!err.response) console.warn(err)
+//             else {
+//                 if (err.response.data.error.message) {
+//                     if (postProcesError) postProcesError(err.response.data.error.message)
+//                 }
+//             }
+//         })
+//     }
+// }
