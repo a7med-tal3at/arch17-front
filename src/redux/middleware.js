5e045d8f51b20c8ae7bf0e0776e11dc4598e6bef
gom3a <mohamed.mido85@azhar.edu.eg>
2021-07-12 11:32:41 +0200

    committing

--- src/redux/middleware.js
+++ src/redux/middleware.js
@@ -13,7 +13,6 @@ if (action.type===constants.API){
         data: data ? data : null
     }).then((response) => {
         // console.log(response)
-
         window.location.reload('/')
         if (success) dispatch(success(response.data));
         if (success) postProcesSuccess(response.data);
@@ -27,29 +26,4 @@ if (action.type===constants.API){
     })
 }else{
     return next(action);
-}
-}
-
-// export const AuthRequest = ()=>{
-//     return dispatch => action => {
-//         const { url, method, success, data, postProcesSuccess, postProcesError } = action.payload;
-      
-//         dispatch(registerUser())
-//         axios({
-//             method,
-//             url: BASE_URL + url,
-//             data: data ? data : null
-//         }).then((response) => {
-//             window.location.reload('/')
-//             if (success) dispatch(success(response.data));
-//             if (success) postProcesSuccess(response.data);
-//         }).catch(err => {
-//             if (!err.response) console.warn(err)
-//             else {
-//                 if (err.response.data.error.message) {
-//                     if (postProcesError) postProcesError(err.response.data.error.message)
-//                 }
-//             }
-//         })
-//     }
-// }
+}}
